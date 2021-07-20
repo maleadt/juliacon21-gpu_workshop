c3afd329cefd4f09361b97bf705f0623957c7fe8
Valentin Churavy <v.churavy@gmail.com>
2021-07-20 05:29:47 -0400

    indentation for common

--- common_definitions.jl
+++ common_definitions.jl
@@ -4,20 +4,20 @@ Pkg.activate(@__DIR__)
 using KernelAbstractions
 
 if GPU_PKG_NAME == "CUDA"
-using CUDA, CUDAKernels
-const GPUMOD = CUDA
-const GpuArray = CuArray
-const GpuBackend = CuDevice()
+    using CUDA, CUDAKernels
+    const GPUMOD = CUDA
+    const GpuArray = CuArray
+    const GpuBackend = CuDevice()
 elseif GPU_PKG_NAME == "AMDGPU"
-using AMDGPU, ROCKernels
-const GPUMOD = AMDGPU
-const GpuArray = ROCArray
-const GpuBackend = ROCDevice()
+    using AMDGPU, ROCKernels
+    const GPUMOD = AMDGPU
+    const GpuArray = ROCArray
+    const GpuBackend = ROCDevice()
 elseif GPU_PKG_NAME == "oneAPI"
 using oneAPI
-const GPUMOD = oneAPI
-const GpuArray = ZeArray
-const GpuBackend = CPU()
+    const GPUMOD = oneAPI
+    const GpuArray = ZeArray
+    const GpuBackend = CPU()
 end
 
 using ImageShow
